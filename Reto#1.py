ab4003f307c6f2cf3bad42d6eb80d6f05c9de7c6
Andrés Echenique <aechenique91@gmail.com>
2024-02-05 11:02:37 -0500

    Reto día 1

---
+++ Reto#1.py
@@ -0,0 +1,14 @@
+import os
+
+nombres = str(input("Ingrese su/s nombre/s: \n"))
+
+apellidos = str(input("Ingrese su/s apellido/s: \n"))
+
+numero_de_telefono = str(input("Ingrese su teléfono: \n"))
+
+correo_electronico= str(input("Ingrese su correo electrónico: \n"))
+
+print(f"Hola {nombres} {apellidos}, en breve recibirás un correo a {correo_electronico} .")
+
+
+
